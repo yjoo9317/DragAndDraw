14d9e89feaece6c63d874c86622f341b1d3df351
Jason Ryu and Shawn Kang <pair+jaeyeon.ryu+sh0708.kang@samsung.com>
2017-11-15 17:03:24 +0900

    add basic touch event handler

--- app/src/main/java/com/youngjoo/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/youngjoo/draganddraw/BoxDrawingView.java
@@ -1,7 +1,10 @@
 package com.youngjoo.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -9,6 +12,7 @@ import android.view.View;
  */
 
 public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
 
     public BoxDrawingView(Context context){
         this(context, null);
@@ -16,5 +20,27 @@ public class BoxDrawingView extends View {
 
     public BoxDrawingView(Context context, AttributeSet attributeSet){
         super(context, attributeSet);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN :
+                action = "Action_Down";
+                break;
+            case MotionEvent.ACTION_MOVE :
+                action = "Action_Move";
+                break;
+            case MotionEvent.ACTION_UP :
+                action = "Action_Up";
+                break;
+            case MotionEvent.ACTION_CANCEL :
+                action = "Action_Cancel";
+                break;
+        }
+        Log.i(TAG, action+" at "+ "("+current.x+", "+current.y+")");
+        return true;
     }
 }
